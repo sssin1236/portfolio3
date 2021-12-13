780e022f90b5408de7be60f9604abd298401b9a6
sssin1236 <suj0358@gmail.com>
2021-12-14 02:09:16 +0900

    class on 제거 오류

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const visual = document.querySelector("#visual");
+const right = visual.querySelector(".right");
+const boxs = right.querySelectorAll("article");
+const clos = document.querySelector("article .close");
+console.log(clos);
+
+boxs.forEach((box, index)=>{
+    box.addEventListener("click", e=>{
+        for(let el of boxs){
+            el.classList.remove("on");
+            el.classList.add("off");
+        } 
+        boxs[index].classList.remove("off");
+        boxs[index].classList.add("on");
+    });
+});
+
