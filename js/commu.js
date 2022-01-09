2dea81836a11e2eb30f985e94476ace833953a9c
sssin1236 <suj0358@gmail.com>
2022-01-10 01:13:50 +0900

    tab event 방지

--- js/commu.js
+++ js/commu.js
@@ -2,12 +2,25 @@ const menu2 = document.querySelector("#menu2");
 const menu3 = document.querySelector("#menu3");
 const dts1 = menu2.querySelectorAll("dt");
 const dds1 = menu2.querySelectorAll("dd");
+const dts2 = menu3.querySelectorAll("dt");
+const dds2 = menu3.querySelectorAll("dd");
 const numbers = document.querySelectorAll("#pagination .numbers a");
 
 dts1.forEach((dt, index)=>{
     dt.addEventListener("click", e=>{
         for(let el of dts1) el.classList.remove("on");
         for(let el of dds1) el.style.display = "none";
+        dts1[index].classList.add("on");
+
+        let target = e.currentTarget.nextElementSibling;
+        target.style.display = "block";
+    });
+});
+
+dts2.forEach((dt, index)=>{
+    dt.addEventListener("click", e=>{
+        for(let el of dts2) el.classList.remove("on");
+        for(let el of dds2) el.style.display = "none";
         dts1[index].classList.add("on");
 
         let target = e.currentTarget.nextElementSibling;
@@ -29,6 +42,12 @@ class Tab {
         this.main = document.querySelector(".menu");
         this.btns = document.querySelectorAll(".menu>dt");
         this.boxs = document.querySelectorAll(".menu>dd");
+        this.menu2 = document.querySelector("#menu2");
+        this.menu3 = document.querySelector("#menu3");
+        this.dts1 = this.menu2.querySelectorAll("dt");
+        this.dds1 = this.menu2.querySelectorAll("dd");
+        this.dts2 = this.menu3.querySelectorAll("dt");
+        this.dds2 = this.menu3.querySelectorAll("dd");
         this.speed = 1000;
         this.enableClick = true;
         console.log(this.boxs);
@@ -44,6 +63,10 @@ class Tab {
 
                     this.activation(this.btns, index);
                     this.activation(this.boxs, index);
+                    for(let el of this.dts1) el.classList.remove("on")
+                    for(let el of this.dts2) el.classList.remove("on")
+                    for(let el of this.dds1) el.style.display = "none";
+                    for(let el of this.dds2) el.style.display = "none";
 
                     setTimeout(this.enableClick = true, 1000);
                 }
