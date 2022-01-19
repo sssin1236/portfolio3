8232c60eed8fb6010569544c6be06fd6a9f0f9c3
sssin1236 <suj0358@gmail.com>
2022-01-19 23:16:52 +0900

    style

--- js/youtube.js
+++ js/youtube.js
@@ -3,13 +3,12 @@ const key = "AIzaSyCCiZuUxyRnAnWNnLdQxnZ5COuFx0Cv33A";
 const playlist_Id = "PLlgbG45RVNaqmozynV0bmmtSQtFrMNNVv";
 const num = 3;
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&part=snippet&playlistId=${playlist_Id}&maxResults=${num}`
-console.log(main);
+
 fetch(url)
 .then(data=>{
     return data.json();
 })
 .then(json=>{
-    console.log(json.items);
     const items = json.items;
 
     let result = "";
@@ -21,14 +20,19 @@ fetch(url)
         let desc = item.snippet.description;
         let date = item.snippet.publishedAt.split("T")[0];
 
+        if(tit.length > 50) tit = tit.substr(0, 50)+".....";
+        if(desc.length > 150) desc = desc.substr(0, 150)+".....";
+
         result += `
             <article>
-                <a href="#">
-                    <img src="${item.snippet.thumbnails.standard.url}"
+                <a href="${item.snippet.resourceId.videoId}" class="pic">
+                    <img src="${item.snippet.thumbnails.standard.url}">
                 </a>
-                <h2>${tit}</h2>
-                <p>${desc}</p>
-                <span>${date}</span>
+                <div class="con">
+                    <h2>${tit}</h2>
+                    <p>${desc}</p>
+                    <span>${date}</span>
+                </div>
             </article>
         `;
     })
