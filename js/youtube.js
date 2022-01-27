dd980ca8289f1fc1d376774c9ae3077cf32b78fe
sssin1236 <suj0358@gmail.com>
2022-01-28 02:57:52 +0900

    함수 패키징

--- js/youtube.js
+++ js/youtube.js
@@ -1,7 +1,7 @@
 
 class Youtube{
     constructor(){
-        
+
     }
 }
 
@@ -12,45 +12,54 @@ const playlist_Id = "PLlgbG45RVNaqmozynV0bmmtSQtFrMNNVv";
 const num = 5;
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&part=snippet&playlistId=${playlist_Id}&maxResults=${num}`
 
-fetch(url)
-.then(data=>{
-    return data.json();
-})
-.then(json=>{
-    const items = json.items;
+createList1(url);
 
-    let result = "";
+main.addEventListener("click", e=>createPop(e));
 
-    items.map( item=>{
+frame.addEventListener("click", e=>removePop(e))
 
-        let tit = item.snippet.title;
-        let desc = item.snippet.description;
-        let chanel = item.snippet.videoOwnerChannelTitle;
-        let date = item.snippet.publishedAt.split("T")[0];
-
-        if(tit.length > 30) tit = tit.substr(0, 30)+".....";
-        if(desc.length > 150) desc = desc.substr(0, 150)+".....";
-
-        result += `
-            <article>
-                <a href="${item.snippet.resourceId.videoId}" class="pic">
-                    <img src="${item.snippet.thumbnails.standard.url}">
-                </a>
-                <div class="con">
-                    <span>${date}</span>
-                    <h2>${tit}</h2>
-                    <nav>${chanel}</nav>
-                    <p>${desc}</p>
-                    <a href="#" class="view">VIDEO PLAY</a>
-                </div>
-            </article>
-        `;
+function createList1(url){
+    fetch(url)
+    .then(data=>{
+        return data.json();
     })
+    .then(json=>{
+        const items = json.items;
 
-    main.innerHTML = result;
+        let result = "";
+
+        items.map( item=>{
+
+            let tit = item.snippet.title;
+            let desc = item.snippet.description;
+            let chanel = item.snippet.videoOwnerChannelTitle;
+            let date = item.snippet.publishedAt.split("T")[0];
+
+            if(tit.length > 30) tit = tit.substr(0, 30)+".....";
+            if(desc.length > 150) desc = desc.substr(0, 150)+".....";
+
+            result += `
+                <article>
+                    <a href="${item.snippet.resourceId.videoId}" class="pic">
+                        <img src="${item.snippet.thumbnails.standard.url}">
+                    </a>
+                    <div class="con">
+                        <span>${date}</span>
+                        <h2>${tit}</h2>
+                        <nav>${chanel}</nav>
+                        <p>${desc}</p>
+                        <a href="#" class="view">VIDEO PLAY</a>
+                    </div>
+                </article>
+            `;
+        })
+
+        main.innerHTML = result;
 })
 
-main.addEventListener("click", e=>{
+}
+
+function createPop(e){
     e.preventDefault();
     let vidId = '';
 
@@ -72,12 +81,12 @@ main.addEventListener("click", e=>{
     `;
 
     frame.append(pop);
-});
+}
 
-frame.addEventListener("click", e=>{
+function removePop(e){
     const pop = frame.querySelector("aside");
     if(pop == null) return;
 
     const close = pop.querySelector("span");
     if(e.target == close) e.target.closest("aside").remove();
-})
+}
