368c1d2890db1653fe6a909eb80200680818ae21
sssin1236 <suj0358@gmail.com>
2022-02-04 23:51:17 +0900

    brand scroll

--- js/brand.js
+++ js/brand.js
@@ -2,11 +2,60 @@ const member = document.querySelector("#member");
 const slider = member.querySelector(".slider");
 const ul = slider.querySelector(".shef");
 const lis = ul.querySelectorAll("li");
+const mains = querySelectorAll("main");
 let num= 0;
 
 init()
 
 timer = setInterval(move, 50);
+
+// window.addEventListener("mousewheel", e=>{
+
+//     let activeIndex = scroll_arr.indexOf(activeItem);
+//     let targetIndex;
+//     let isOn = sections[1].classList.contains("on");
+
+//     if(e.deltaY < 0){
+//         if(activeIndex == 0) return;
+//         targetIndex = activeIndex -1;
+//     }else{
+//         if(activeIndex == len-1) return;
+//         targetIndex = activeIndex +1;
+//     }
+
+//     if(isOn){
+//         for(let el of boxs){
+//             el.classList.add("off")
+//         } 
+//     }else{
+//         for(let el of boxs){
+//             setTimeout(function(){
+//                 el.classList.remove("off")
+//             }, 1000);
+//         } 
+//     }
+
+//     new Anim(window,{
+//         prop: "scroll",
+//         value: posArr[targetIndex],
+//         duration: speed
+//     });
+// }, {passive : false});
+
+// // window.addEventListener("scroll", e=>{
+// //     let scroll = window.scrollY || window.pageYOffset;
+
+// //     sections.forEach((el, index)=>{
+// //         if(scroll >= posArr[index] + base){
+// //             scrollBtn.forEach((el, i)=>{
+// //                 el.classList.remove("on");
+// //                 sections[i].classList.remove("on");
+// //             })
+// //             scrollBtn[index].classList.add("on");
+// //             sections[index].classList.add("on");
+// //         }
+// //     });
+// // });
 
 ul.addEventListener("mouseenter", e=>{
     clearInterval(timer);
@@ -30,4 +79,10 @@ function move(){
 function init(){
     ul.prepend(ul.lastElementChild);
     ul.style.marginLeft = 220;
+}
+
+function setPos(){
+    posArr = [];
+    
+    for(let el of sections) posArr.push(el.offsetTop);
 }
